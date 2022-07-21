97632a95bde4044835ee988d3c824d580d146733
Vadim Rodionov <rodionov@tuta.io>
2022-07-21 17:45:04 +0600

    Add solution for card game war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,55 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
-(def cards
+(defonce cards
   (for [suit suits
         rank ranks]
     [suit rank]))
+(defonce card-values
+  (->> ranks
+       (map-indexed (fn [i e] {e i}))
+       (reduce merge {})))
 
-(defn play-round [player1-card player2-card])
+(defn play-round
+  "Returns :card1 if first card is greater in value, :card2 if opposite, or nil
+  if cards are equal in value"
+  [[_ rank1] [_ rank2]]
+  (let [card1-value (get card-values rank1)
+        card2-value (get card-values rank2)]
+    (cond
+      (> card1-value card2-value) :card1
+      (< card1-value card2-value) :card2)))
 
-(defn play-game [player1-cards player2-cards])
+(defn- game [ac1 d1 ac2 d2]
+  (let [c1 (first ac1)
+        c2 (first ac2)]
+    (cond
+      (or (and (empty? d1) (empty? d2))
+          (and (not c1) (not c2)))
+      "It's a draw"
+
+      (or (not c1) (empty? d1)) "Player 2 won"
+
+      (or (not c2) (empty? d2)) "Player 1 won"
+
+      :else
+      (case (play-round c1 c2)
+        :card1 (game [(first d1)]
+                     (concat (rest d1) ac1 ac2)
+                     [(first d2)]
+                     (rest d2))
+        :card2 (game [(first d1)]
+                     (rest d1)
+                     [(first d2)]
+                     (concat (rest d2) ac2 ac1))
+        nil    (game (concat (reverse (take 4 d1)) ac1)
+                     (drop 4 d1)
+                     (concat (reverse (take 4 d2)) ac2)
+                     (drop 4 d2))))))
+
+(defn play-game [player1-cards player2-cards]
+  (game [(first player1-cards)]
+        (rest player1-cards)
+        [(first player2-cards)]
+        (rest player2-cards)))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -1,16 +1,22 @@
 (ns card-game-war.game-test
-  (:require [clojure.test :refer :all]
-            [card-game-war.game :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [card-game-war.game :refer [play-round play-game cards suits]]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+    (doseq [suit1 suits rank1 (range 2 6) suit2 suits rank2 (range 6 11)]
+      (is (= :card2 (play-round [suit1 rank1] [suit2 rank2])))))
+  (testing "queens are higher rank than jacks"
+    (doseq [suit1 suits suit2 suits]
+      (is (= :card1 (play-round [suit1 :queen] [suit2 :jack])))))
+  (testing "kings are higher rank than queens"
+    (doseq [suit1 suits suit2 suits]
+      (is (= :card1 (play-round [suit1 :king] [suit2 :queen])))))
+  (testing "aces are higher rank than kings"
+    (doseq [suit1 suits suit2 suits]
+      (is (= :card1 (play-round [suit1 :ace] [suit2 :king]))))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (let [cardss (shuffle cards)]
+      (play-game (take 26 cardss) (drop 26 cardss)))))
