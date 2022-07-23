9855bf16834dae08709017f7b33ef453add0b151
Vadim Rodionov <rodionov@tuta.io>
2022-07-23 15:40:45 +0600

    Add solution for 'doublets'

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -2,10 +2,35 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(def words (-> "words.edn"
-               (io/resource)
-               (slurp)
-               (read-string)))
+(defonce words (-> "words.edn"
+                   (io/resource)
+                   (slurp)
+                   (edn/read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+(defn- count-diff [w1 w2]
+  (reduce + (map #(if (= %1 %2) 0 1) w1 w2)))
+
+(defn- chain [chain-so-far words goal]
+  (let [last (last chain-so-far)
+        candidates (filter #(= 1 (count-diff last %)) words)]
+    (cond
+      (= 1 (count-diff goal last))
+      (conj chain-so-far goal)
+
+      (empty? candidates)
+      []
+
+      :else
+      (nth (filter seq
+                   (lazy-seq
+                    (map #(chain (conj chain-so-far %)
+                                 (remove #{%} words)
+                                 goal)
+                         candidates)))
+           0 []))))
+
+(defn doublets [w1 w2]
+  (let [length (count w1)
+        words (filter #(and (= length (count %)) #{w1})
+                      words)]
+    (chain [w1] words w2)))
