18351c098fab3b064e6ceca625368c62c7fc0c50
Vadim Rodionov <rodionov@tuta.io>
2022-07-28 17:19:16 +0600

    Add solution for 'magic_square'

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,7 +2,41 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+(defn- permutate [coll]
+  (if (= 1 (count coll))
+    (list coll)
+    (for [head coll
+          tail (permutate (disj (set coll) head))]
+      (cons head tail))))
+
+(defn- sum-rows [m]
+  (map #(reduce + %) m))
+
+(defn- sum-cols [m]
+  [(reduce + (map first m))
+   (reduce + (map second m))
+   (reduce + (map last m))])
+
+(defn- sum-diagonals [m]
+  [(+ (get-in m [0 0]) (get-in m [1 1]) (get-in m [2 2]))
+   (+ (get-in m [2 0]) (get-in m [1 1]) (get-in m [0 2]))])
+
+(defn- magic-square? [square]
+  (and
+   (= (set (sum-rows square))
+      (set (sum-cols square))
+      (set (sum-diagonals square)))
+   (= 1
+      (count (set (sum-rows square)))
+      (count (set (sum-cols square)))
+      (count (set (sum-diagonals square))))))
+
+(defn- to-vec [c]
+  (vec (map vec c)))
+
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (->> (permutate values)
+       (map #(partition 3 %))
+       (map to-vec)
+       (filter magic-square?)
+       first))
