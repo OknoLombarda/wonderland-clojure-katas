a5f30186d39987a77db893bcb4634a0d8f35ac08
Vadim Rodionov <rodionov@tuta.io>
2022-08-08 13:08:34 +0600

    Add solution for 'wonderlan-number'

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [n1 n2]
+  (let [n1-chars (group-by identity (str n1))
+        n2-chars (group-by identity (str n2))]
+    (= n1-chars n2-chars)))
+
+(defn wonderland-number? [num]
+  (let [products (for [m (range 2 7)]
+                   (* m num))]
+    (every? #(same-digits? num %) products)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [upper-limit (int (java.lang.Math/ceil (/ 1000000.0 6)))
+        candidates (range 100000 upper-limit)]
+    (first (filter wonderland-number? candidates))))
